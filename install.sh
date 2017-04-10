4187a1becde25a1c1e4b80268cbe897139eb8e7e
Juriy Zaytsev <kangax@gmail.com>
2017-04-10 14:28:21 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -194,14 +194,14 @@ wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | 
 
 echo "Installing global node packages..."
 node_packages=(
-  bower
+  # bower
   browserify
   caniuse-cmd
   grunt
   gulp
-  htmlhint
+  # htmlhint
   imageoptim-cli
-  jshint
+  # jshint
   jscs
   qunit
   watchify
